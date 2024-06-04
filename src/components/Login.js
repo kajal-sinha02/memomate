e26bcc30d175535234a43377ebbfbb65d63dd453
kajal sinha <kajal.2022ug1049@iiitranchi.ac.in>
2024-06-04 12:43:29 +0530

    ..

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate ,Link} from "react-router-dom";
 import Alert from "./Alert";
 
 const Login = (props) => {
